d9835b93680891d6e6660c35cb5f4f4840362c4a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2024-01-10 15:55:11 -0500

    Introduce RedactedData
    Ruby 3.2 introduced [Data](https://docs.ruby-lang.org/en/3.2/Data.html). This commit adds support for `RedactedData` where `Data` is available.

--- lib/redacted_struct.rb
+++ lib/redacted_struct.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
-# A subclass of Struct that redacts members by default, and can allow some to be printed
-class RedactedStruct < Struct
-  VERSION = "1.1.0"
+# Class methods for the Redactable module. When Redactable is included in class that class is
+# extended with these instance methods.
+module RedactableClassMethods
+  attr_reader :allowed_members
+end
 
-  def self.new(*name_and_members, keyword_init: nil, allowed_members: [], &block)
-    super(*name_and_members, keyword_init: keyword_init, &block).tap do |struct_class|
-      struct_class.class_eval do
-        @allowed_members = Array(allowed_members)
-      end
-    end
-  end
-
-  class << self
-    attr_reader :allowed_members
-  end
-
-  def allowed_members
-    self.class.allowed_members
+# A module that adds redacted functionality to a class
+module Redactable
+  def self.included(mod)
+    mod.extend RedactableClassMethods
   end
 
   def inspect
@@ -25,23 +17,25 @@ class RedactedStruct < Struct
 
     attributes = members.map do |member|
       if allowed_members.include?(member)
-        "#{member}=#{self[member].inspect}"
+        "#{member}=#{send(member).inspect}"
       else
         "#{member}=[REDACTED]"
       end
     end.join(" ")
 
-    "#<struct#{name_or_nil} #{attributes}>"
+    "#<#{base_name_for_inspection}#{name_or_nil} #{attributes}>"
   end
 
-  alias_method :to_s, :inspect
+  def allowed_members
+    self.class.allowed_members
+  end
 
   # Overrides for pp
   # See https://github.com/ruby/pp/blob/3d925b5688b8226f653127d990a8dce48bced5fe/lib/pp.rb#L379-L391
   # rubocop:disable all
   def pretty_print(q)
     q.group(1, sprintf("#<struct %s", PP.mcall(self, Kernel, :class).name), '>') {
-      q.seplist(PP.mcall(self, Struct, :members), lambda { q.text "," }) {|member|
+      q.seplist(PP.mcall(self, base_type_for_pp, :members), lambda { q.text "," }) {|member|
         q.breakable
         q.text member.to_s
         q.text '='
@@ -57,4 +51,74 @@ class RedactedStruct < Struct
     }
   end
   # rubocop:enable all
+
+  alias_method :to_s, :inspect
+
+  def base_name_for_inspection
+    if is_a? Struct
+      "struct"
+    elsif is_a? Data
+      "data"
+    end
+  end
+
+  def base_type_for_pp
+    if is_a? Struct
+      Struct
+    elsif is_a? Data
+      Data
+    end
+  end
 end
+
+# A subclass of Struct that redacts members by default, and can allow some to be printed
+class RedactedStruct < Struct
+  include Redactable
+
+  VERSION = "1.1.0"
+
+  def self.new(*name_and_members, keyword_init: nil, allowed_members: [], &block)
+    super(*name_and_members, keyword_init: keyword_init, &block).tap do |struct_class|
+      struct_class.class_eval do
+        @allowed_members = Array(allowed_members)
+      end
+    end
+  end
+end
+
+# rubocop:disable Style/MultilineIfModifier
+# A sublcass of Data that redacts members by default, and can allow some to be printed
+class RedactedData < Data
+  include Redactable
+
+  def self.define(*members, allowed_members: [], &block)
+    super(*members, &block).tap do |data_class|
+      data_class.class_eval do
+        @allowed_members = Array(allowed_members)
+      end
+    end
+  end
+
+  # Overrides for pp
+  # See https://github.com/ruby/pp/blob/3d925b5688b8226f653127d990a8dce48bced5fe/lib/pp.rb#L379-L391
+  # rubocop:disable all
+  def pretty_print(q)
+    q.group(1, sprintf("#<data %s", PP.mcall(self, Kernel, :class).name), '>') {
+      q.seplist(PP.mcall(self, Data, :members), lambda { q.text "," }) {|member|
+        q.breakable
+        q.text member.to_s
+        q.text '='
+        q.group(1) {
+          q.breakable ''
+          if allowed_members.include?(member)
+            q.pp self.send(member)
+          else
+            q.text "[REDACTED]"
+          end
+        }
+      }
+    }
+  end
+  # rubocop:enable all
+end if defined?(Data)
+# rubocop:enable Style/MultilineIfModifier
